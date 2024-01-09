a85b03b731cbba19b24ee24349c50dcdf17fb755
Akinnikemichael <michaelaakinnike@gmail.com>
2024-01-09 12:24:35 +0100

    7th commit

---
+++ class_employee.py
@@ -0,0 +1,9 @@
+class employee:
+    def __init__(self, name, department, level, bank_name, account_number, is_today_last_day_of_the_month):
+        self.name = name
+        self.department = department
+        self.level = level
+        self.bank_name = bank_name
+        self.account_number = account_number
+        self.is_today_last_day_of_the_month = is_today_last_day_of_the_month
+
